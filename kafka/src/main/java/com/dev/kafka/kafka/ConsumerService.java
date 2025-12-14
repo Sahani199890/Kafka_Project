dc849c713e92e45b93c3f588c6768552ed0cf307
Abhishek-Sahani01 <abhishek.sahani@voltuswave.com>
2025-12-14 21:59:20 +0530

    frontend for the async messaging

--- kafka/src/main/java/com/dev/kafka/kafka/ConsumerService.java
+++ kafka/src/main/java/com/dev/kafka/kafka/ConsumerService.java
@@ -1,16 +1,25 @@
 package com.dev.kafka.kafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumerService {
+
+    private final SimpMessagingTemplate template;
+
+    public ConsumerService(SimpMessagingTemplate template){
+        this.template=template;
+    }
+
     @KafkaListener(
             topics = "dev-topic1",
     groupId="dev-consumer-group"
     )
-    public String consumeMessage(String message){
+    public void consumeMessage(String message){
         System.out.println("Consumed message: "+ message);
-        return message;
+        template.convertAndSend("/topic/kafka-messages",message);
+//        return message;
     }
 }
